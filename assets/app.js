3c524574acc58f671afdec9937627f645de9d5e9
DiegoHuacanjulca <diant2405@gmail.com>
2021-08-22 16:03:50 -0500

    Optimize method, rename showProducts & showCategories

--- assets/app.js
+++ assets/app.js
@@ -1,59 +1,130 @@
-const API_CATEGORIES = "https://shop-rest-api.test.com/api/categorias";
-const API_PRODUCTS = "https://shop-rest-api.test.com/api/productos";
+const API_CATEGORIES = "https://shop-rest-api.test.com/api/categories";
+const API_PRODUCTS = "https://shop-rest-api.test.com/api/products";
+const category_id = null;
 
 const htmlCategories = document.querySelector("#categories");
 const htmlProducts = document.querySelector("#products");
 
+addElementCategory();
+addElementProducts(getProductsApiUrl(category_id));
 
+document.addEventListener('click',function(e){
+	if(e.target.tagName.toLowerCase() === 'li'){
+	    let categoryId = e.target.value;
+	    addElementProducts(getProductsApiUrl(categoryId));
+	}
+})	
 
-fetch(API_CATEGORIES)
+function getProductsApiUrl(categoryId){
+	if(categoryId){
+		return API_PRODUCTS+'?category_id='+categoryId;
+	}
+	return API_PRODUCTS;
+}
+
+function addElementCategory(){
+	fetch(API_CATEGORIES)
 	.then((response) => response.json())
 	.then((categories) => {
 		for (category of categories){
-	    	let li = document.createElement('li');
-	    	li.className = "list-group-item btn btn-primary text-left";
-
-			li.appendChild(
-				document.createTextNode(category.name)
-			);
-			htmlCategories.appendChild(li);
+			renderCategory(category);	    	
 	    } 
-	});
+	});	
+}
 
-fetch(API_PRODUCTS)
+function addElementProducts(apiURl){	
+	console.log(apiURl)
+	fetch(apiURl)
 	.then((response) => response.json())
-	.then((products) => {
+	.then((products) => {		
+		htmlProducts.innerHTML = "";		
 		for(product of products){
-			
-			let templateProduct = document.querySelector("#templateProduct");
-			let contentTemplateProduct = document.importNode(templateProduct.content, true);
-
-			const formatCurrency = new Intl.NumberFormat('es-PE', {
-				style: 'currency',
-			    currency: 'PEN',
-			    minimumFractionDigits: 2
-			});
-
-			contentTemplateProduct.querySelector('h3.card-title').innerHTML = product.name;
-			
-			if(product.url_image == null || product.url_image ==="")
-				contentTemplateProduct.querySelector('img.card-img-top').src = 'assets/images/no_image.png';
-			else
-				contentTemplateProduct.querySelector('img.card-img-top').src = product.url_image;
-			
-			contentTemplateProduct.querySelector('img.card-img-top').alt = product.name;
-			contentTemplateProduct.querySelector('img.card-img-top').title = product.name;
-			contentTemplateProduct.querySelector('span.price').innerHTML = formatCurrency.format(product.price);
-
-			if(product.discount == 0)
-				contentTemplateProduct.querySelector('div.discount').className = "d-none"
-			else
-				contentTemplateProduct.querySelector('div.discount').innerHTML = product.discount+'%';
-
-			htmlProducts.appendChild(contentTemplateProduct);
+			renderProduct(product);
 		}
-	});
+	});	
+}
+
+function renderCategory(category) {	
+	let elementTemplate = document.querySelector("#templateCategory");
+	let template = document.importNode(elementTemplate.content, true);
+
+	renderCategoryName(template, category);
+
+	htmlCategories.appendChild(template);
+}
 
 
-
+function renderProduct(product) {	
+	let elementTemplate = document.querySelector("#templateProduct");
+	let template = document.importNode(elementTemplate.content, true);
 	
+	renderProductImage(template, product);
+	renderProductTitle(template, product.name);
+	renderProductCategory(template, product.category);
+	renderProductPrice(template, product.price);
+	renderProductDiscount(template, product.discount);
+
+	htmlProducts.appendChild(template);
+}
+
+function renderCategoryName(template, category){
+	template.querySelector('li').innerHTML = category.name;
+	template.querySelector('li').value = category.id;
+}
+
+function renderProductImage(template, product){
+	template.querySelector('img.card-img-top').src = getImageUrl(product.url_image);
+	template.querySelector('img.card-img-top').alt = product.name;
+	template.querySelector('img.card-img-top').title = product.name;
+}
+
+function renderProductTitle(template, name){
+	template.querySelector('h3.card-title').innerHTML = name;
+}
+
+function renderProductCategory(template, category){
+	template.querySelector('a.category').innerHTML = category;
+}
+
+function renderProductPrice(template, price){
+	template.querySelector('span.price').innerHTML = getFormattedPrice(price);
+}
+
+function renderProductDiscount(template, discount){
+	if(discount > '0'){
+		template.querySelector('div.discount').innerHTML = getFormattedDiscount(discount);
+	}else{	
+		template.querySelector('div.discount').className = "d-none"
+	}
+}
+/**
+ * Esta función recibe un producto y devuelve la URL de su imagen.
+ * El valor es el mismo a menos que no tenga, y se use una imagen por defecto.
+ */
+function getImageUrl(url_image) {
+	if (url_image) {
+		return url_image;
+	}
+	return 'assets/images/no_image.png';
+}
+
+/**
+ * Esta función recibe un precio como flotante (ej. 34.5456456)
+ * y devuelve una cadena lista para mostrar al usuario final (ej. $ 34.54)
+ */
+function getFormattedPrice(price) {	
+	const formatCurrency = new Intl.NumberFormat('es-PE', {
+		style: 'currency',
+	    currency: 'PEN',
+	    minimumFractionDigits: 2
+	});
+	return formatCurrency.format(price);
+}
+
+function getFormattedDiscount(discount) {	
+	return discount+'%';
+}
+
+
+
+
